1ffd1a91eac8a9df96ecc542f6d2b3aa32fe9ee5
Michał Wójcik <michal.wojcik@eti.pg.edu.pl>
2024-02-05 17:29:27 +0100

    refactor: slideshow script methods extraction and comments

--- src/js/slideshow.js
+++ src/js/slideshow.js
@@ -3,18 +3,12 @@ let slideshow;
 let slides = window.location.pathname;
 
 if (slides === '/') {
-    window.location.replace("/slides/Materials.md");
+    window.location.replace('/slides/Materials.md');
 }
 
-let presentationCustomStyles = document.createElement("link");
-console.log(slides);
-const base = slides.split('/').slice(2, -1).join('/');
-const css = `/css/slides/${base}/styles.css`;
-presentationCustomStyles.href = css;
-presentationCustomStyles.rel = "stylesheet";
-presentationCustomStyles.type = "text/css";
-document.head.appendChild(presentationCustomStyles);
+appendCustomSlides(slides);
 
+// Fetch markdown slides.
 let slidesRequest = $.ajax({
     url: slides,
     type: 'GET',
@@ -23,6 +17,7 @@ let slidesRequest = $.ajax({
     }
 });
 
+// Fetch slides template.
 let templateRequest = $.ajax({
     url: '/md/template.md',
     type: 'GET',
@@ -32,41 +27,61 @@ let templateRequest = $.ajax({
 });
 
 $.when(slidesRequest, templateRequest).done(function (slide, template) {
+    // Markdown source is made with template and appended slides source.
     let md = template[0] + slide[0];
     $('#source').text(md);
+
+    // Create slideshow.
     slideshow = remark.create({}, event => {
+        // Add each slide title (h3 header) as data-title attribute, so it can be used in CSS selectors.
         $('.remark-slide-container').each(function () {
             $(this).attr('data-title', $(this).find('h3').text());
         });
     });
 
+    // Initialize mermaid diagram engine.
     mermaid.initialize({
         startOnLoad: false,
         cloneCssStyles: false,
         theme: 'neutral'
     });
 
-    function initMermaid(s) {
-        let diagrams = document.querySelectorAll('pre > .mermaid');
-        for (let i = 0; i < diagrams.length; i++) {
-            let diagramText = '';
-            $(diagrams[i]).children('div').each(function () {
-                diagramText += this.innerText + '\n'
-            });
-            let mermaidDiagram = $('<div class="mermaid"></div>');
-            mermaidDiagram.text(diagramText);
-            $(diagrams[i]).parent().replaceWith(mermaidDiagram);
-        }
+    renderMermaidDiagrams(); // Render mermaid diagram when displaying slide (e.g.: by direct link).
+    slideshow.on('afterShowSlide', renderMermaidDiagrams); // Render mermaid diagram when navigating to next slide.
+});
 
-        diagrams = document.querySelectorAll('.mermaid');
-        for (let i = 0; i < diagrams.length; i++) {
-            if (diagrams[i].offsetWidth > 0) {
-                mermaid.init(undefined, diagrams[i]);
-            }
-        }
+/**
+ * Creates link element pointing to slides custom CSS and adds it to DOM.
+ */
+function appendCustomSlides(path) {
+    let customStyles = document.createElement('link');
+    const base = path.split('/').slice(2, -1).join('/');
+    customStyles.href = `/css/slides/${base}/styles.css`;
+    customStyles.rel = 'stylesheet';
+    customStyles.type = 'text/css';
+    document.head.appendChild(customStyles);
+}
+
+/**
+ * Iterates through all pre elements containing element with mermaid class and replaces them with div elements with
+ * mermaid class. Then for each div with mermaid class calls mermaid library init method to render diagram.
+ */
+function renderMermaidDiagrams() {
+    let diagrams = document.querySelectorAll('pre > .mermaid');
+    for (let i = 0; i < diagrams.length; i++) {
+        let diagramText = '';
+        $(diagrams[i]).children('div').each(function () {
+            diagramText += this.innerText + '\n'
+        });
+        let mermaidDiagram = $('<div class="mermaid"></div>');
+        mermaidDiagram.text(diagramText);
+        $(diagrams[i]).parent().replaceWith(mermaidDiagram);
     }
 
-    initMermaid(slideshow.getSlides()[slideshow.getCurrentSlideIndex()]);
-    slideshow.on('afterShowSlide', initMermaid);
-
-});
+    diagrams = document.querySelectorAll('.mermaid');
+    for (let i = 0; i < diagrams.length; i++) {
+        if (diagrams[i].offsetWidth > 0) {
+            mermaid.init(undefined, diagrams[i]);
+        }
+    }
+}
