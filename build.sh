83c75105764e62e29f42d5f824c127007be4a43e
Michał Wójcik <michal.wojcik@eti.pg.edu.pl>
2024-02-05 15:53:53 +0100

    refactor: bash script formatting

--- build.sh
+++ build.sh
@@ -11,10 +11,10 @@
 function main() {
     version="$(grep -n "org.opencontainers.image.version" Dockerfile | cut -f2 -d "=" | xargs)"
     docker build \
-      --label "org.opencontainers.image.created=$(date +%Y-%m-%dT%H:%M:%S%:z)" \
-      --label "org.opencontainers.image.ref.name=$(git rev-parse HEAD)" \
-      --label "org.opencontainers.image.revision=$(git rev-parse HEAD)" \
-      -t "kask.eti.pg.edu.pl/slides-ui":"${version}" .
+        --label "org.opencontainers.image.created=$(date +%Y-%m-%dT%H:%M:%S%:z)" \
+        --label "org.opencontainers.image.ref.name=$(git rev-parse HEAD)" \
+        --label "org.opencontainers.image.revision=$(git rev-parse HEAD)" \
+        -t "kask.eti.pg.edu.pl/slides-ui:${version}" .
 }
 
 main "$@"
